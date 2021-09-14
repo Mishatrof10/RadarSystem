3b4931697dff386293579160e93521ea6f47c907
Mishatrof <Mishatrof007@mail.ru>
2021-09-14 14:04:35 +0700

    Добавил глобальные координаты

---
+++ RadarSystem/GlobalPositionSystem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadarSystem
+{
+    class GlobalPositionSystem
+    {
+        double S;
+        double N;
+    }
+}
